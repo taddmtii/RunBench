8a267baf582e2744a48e6c87eaa1b287194d768f
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-12 17:29:48 -0400

    added stubs for each page

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client"; // adjust path to match your generator output
+import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
