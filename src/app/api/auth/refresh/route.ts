e7acb612c91988232e4103612d6aac276e9ec450
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-12 19:32:50 -0400

    implemented refresh auth logic

--- src/app/api/auth/refresh/route.ts
+++ src/app/api/auth/refresh/route.ts
@@ -1,4 +1,38 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import {
+  attachAuthCookies,
+  generateAccessToken,
+  verifyRefreshToken,
+} from "@/lib/auth";
 
-export async function POST(request: NextRequest) {}
+export async function POST(request: NextRequest) {
+  try {
+    const refreshToken = request.cookies.get("refreshToken")?.value;
+    if (refreshToken) {
+      const { userId } = verifyRefreshToken(refreshToken);
+      const user = await prisma.user.findUnique({ where: { id: userId } });
+      if (user === null) {
+        return NextResponse.json(
+          { error: "Cannot find user." },
+          { status: 401 },
+        );
+      }
+      const newAccessToken = generateAccessToken(user);
+      const response = NextResponse.json({ success: true });
+      response.cookies.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 60 * 30, // 30 min in seconds
+        path: "/",
+      });
+      return response;
+    }
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Invalid or expired token" },
+      { status: 401 },
+    );
+  }
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,20 +6,23 @@ import { User } from "../../generated/prisma/client";
 const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
 
+// Returns the JWT itself
 export function generateAccessToken(user: User) {
   return jwt.sign({ userId: user.id }, ACCESS_SECRET, { expiresIn: "30m" });
 }
 
+// Returns the JWT itself
 export function generateRefreshToken(user: User) {
   return jwt.sign({ userId: user.id }, REFRESH_SECRET, { expiresIn: "7d" });
 }
 
+// Returns decoded payload. Something like { userId: "", iat: ---, exp: } etc...
 export function verifyAccessToken(token: string) {
-  return jwt.verify(token, ACCESS_SECRET);
+  return jwt.verify(token, ACCESS_SECRET) as { userId: string };
 }
 
 export function verifyRefreshToken(token: string) {
-  return jwt.verify(token, REFRESH_SECRET);
+  return jwt.verify(token, REFRESH_SECRET) as { userId: string };
 }
 
 // Attaches both access and refresh token as cookies to response.
