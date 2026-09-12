5a21d90646608723faa70b75da6004c371b2afc6
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-12 19:08:06 -0400

    implemented logout.

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,4 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function POST(request: NextRequest) {}
+export async function POST() {
+  const response = NextResponse.json({ success: true });
+
+  response.cookies.set("accessToken", "", { maxAge: 0, path: "/" });
+  response.cookies.set("refreshToken", "", { maxAge: 0, path: "/" });
+
+  return response;
+}
