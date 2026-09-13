4fb7a0c66c51f8c191a9c3fbcb8f1eeee2a69782
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-13 10:47:57 -0400

    properly stubbed api/run

--- src/app/api/run/route.ts
+++ src/app/api/run/route.ts
@@ -1,5 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  const { code } = body;
+  // Send code to Go service.
+  return NextResponse.json({ message: "Sucesss" }, { status: 200 });
 }
