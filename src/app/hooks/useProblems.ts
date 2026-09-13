4cd0f06162f35fae7394fa4ba23402c2d42b4c12
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-13 11:22:27 -0400

    added tanstack query and its associated provider

---
+++ src/app/hooks/useProblems.ts
@@ -0,0 +1,3 @@
+export default function useProblems() {
+  const [];
+}
--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+
+import { useEffect } from "react"
+
+interface Problem {
+  id: String,
+  title: String,
+  description: String
+  difficulty: "EASY" | "MEDIUM" | "HARD",
+  recommendedTimeComplexity: String,
+  submissionCount: number
+}
+
 export default function Problems() {
-    return (
-        <></>
-    )
+
+  useEffect(() => {
+    
+  }, [])
+  return (
+    <></>
+  )
 }
