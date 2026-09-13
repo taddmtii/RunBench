06508dc1b35702c76af4eb56ece2d7ed645026b0
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-13 12:59:01 -0400

    fixed indiviual problem route

--- src/app/api/problems/[problemId]/route.ts
+++ src/app/api/problems/[problemId]/route.ts
@@ -1,18 +1,15 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const id = searchParams.get("id");
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ problemId: string }> },
+) {
+  const { problemId } = await params;
 
-  if (!id) {
-    return NextResponse.json(
-      { message: "Missing 'id' query parameter" },
-      { status: 400 },
-    );
-  }
-
-  const problem = await prisma.problem.findUnique({ where: { id } });
+  const problem = await prisma.problem.findUnique({
+    where: { id: problemId },
+  });
 
   if (!problem) {
     return NextResponse.json({ message: "Problem not found" }, { status: 404 });
