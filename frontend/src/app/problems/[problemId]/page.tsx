0c64af234fac8eb0057b7c5c7ec997c0d1014e4f
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-17 07:10:03 -0400

    implemnted logic for individual problem page

--- frontend/src/app/problems/[problemId]/page.tsx
+++ frontend/src/app/problems/[problemId]/page.tsx
@@ -1,5 +1,40 @@
+"use client"
+
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react"
+import type { Problem } from "../../../../generated/prisma/client";
+
 export default function Problem() {
+    const [problem, setProblem] = useState<Problem | null>(null);
+    const [loading, setLoading] = useState(true);
+    const router = useParams();
+    const { problemId } = router;
+    useEffect(() => {
+        const fetchProblem = async () => {
+        try {
+            const res = await fetch(`/api/problems/${problemId}`)
+        
+        if (!res.ok) {
+            console.error("Could not retrieve problem")
+            return
+        }
+            const data = await res.json()
+            setProblem(data)
+        } catch (e) {
+            console.error("Could not retrieve problem")
+        } finally {
+            setLoading(false)
+        }
+        }
+
+        if (problemId) {
+            fetchProblem()
+        }
+       
+    }, [problemId])
     return (
-        <></>
+        <>
+        {problem?.title}
+        </>
     )
 }
