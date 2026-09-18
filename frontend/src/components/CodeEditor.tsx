4b5510b73ecef4e30c26ba90527677b046f0c658
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-17 21:09:23 -0400

    added dropdown menu to choose language and wired that state into editor

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -2,23 +2,54 @@
 
 import { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "../components/ui/button";
 
 export default function CodeEditor() {
     const editorRef = useRef(null)
-    const [language, setLanguage] = useState(null)
+    const [language, setLanguage] = useState("python")
     const [code, setCode] = useState("")
     
     const handleEditorChange = (value) => {
         setCode(value)
     }
 
+    const LANGUAGES = [
+        { label: "Python", value: "python" },
+        { label: "Typescript", value: "typescript"}
+    ]
+    
+
     return (
+        <div className="flex flex-col items-center gap-2">
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Button variant="outline">
+                        {/* Needed to capitalize first letter of value and monaco only takes lowercase values */}
+                        {language.charAt(0).toUpperCase() + language.slice(1)}
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuGroup>
+                        {LANGUAGES.map((lang) => (
+                            <DropdownMenuItem 
+                                key={lang.value}
+                                onClick={() => setLanguage(lang.value)}
+                                >
+                                {lang.label}
+                            </DropdownMenuItem>
+                        ))}
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
         <Editor 
-            height="400px"
-            width="600px"
+            height="90vh"
+            width="50vw"
             value={code}
             onChange={handleEditorChange} 
             defaultLanguage="python" 
+            language={language}
             defaultValue="// Write your code here" 
             options={{
                 minimap: { enabled: false},
@@ -33,5 +64,6 @@ export default function CodeEditor() {
             }}
             theme="vs-dark" 
         />
+        </div>
     )
 }
