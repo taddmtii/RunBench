37dfc2e43e5e60f891a7b95b3619aa7f3cbff2f4
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-18 21:31:18 -0400

    added run and submit buttons. small fetch on run

--- frontend/src/app/api/run/route.ts
+++ frontend/src/app/api/run/route.ts
@@ -3,6 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { code } = body;
+  if (!code) {
+    return NextResponse.json(
+      { message: "There was a problem with the request" },
+      { status: 400 },
+    );
+  }
   // Send code to Go service.
   return NextResponse.json({ message: "Sucesss" }, { status: 200 });
 }
--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -15,28 +15,48 @@ export default function CodeEditor() {
         { label: "Javascript", value: "javascript"},
     ]
 
+    const handleRunClick = async () => {
+        const res = await fetch("/api/run", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({code: code})
+        })
+        if (!res.ok) {
+            console.error("Something went wrong with Run.")
+            return
+        } 
+        console.log("Run hit successfully")
+        // const data = await res.json();
+    }
+
     return (
-        <div className="flex flex-col items-center gap-2 p-4">
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Button variant="outline">
-                        {/* Needed to capitalize first letter of value and monaco only takes lowercase values */}
-                        {language.charAt(0).toUpperCase() + language.slice(1)}
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuGroup>
-                        {LANGUAGES.map((lang) => (
-                            <DropdownMenuItem 
-                                key={lang.value}
-                                onClick={() => {setLanguage(lang.value)}}
-                                >
-                                {lang.label}
-                            </DropdownMenuItem>
-                        ))}
-                    </DropdownMenuGroup>
-                </DropdownMenuContent>
-            </DropdownMenu>
+        <div className="flex flex-col items-center justify-between gap-2 p-4">
+            <div className="flex">
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant="outline">
+                            {/* Needed to capitalize first letter of value and monaco only takes lowercase values */}
+                            {language.charAt(0).toUpperCase() + language.slice(1)}
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuGroup>
+                            {LANGUAGES.map((lang) => (
+                                <DropdownMenuItem 
+                                    key={lang.value}
+                                    onClick={() => {setLanguage(lang.value)}}
+                                    >
+                                    {lang.label}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <Button className="cursor-pointer" onClick={handleRunClick}>Run</Button>
+                <Button className="bg-green-400 cursor-pointer">Submit</Button>
+            </div>
             <div className="rounded-xl overflow-hidden shadow-2xl border border-gray-700 bg-gray-900">
                 <Editor 
                     height="90vh"
