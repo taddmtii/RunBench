b0e48e9ba8c6fc98cf13cec044c073d6949295d7
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-20 11:02:08 -0400

    fleshed out a little more of the service, as well as boilerplate in main,go

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -1,5 +1,10 @@
 package service
 
+import (
+	"os"
+	"path/filepath"
+)
+
 // Contains main logic for ...
 // Initializing Docker clinet
 // For each test case spin up container, execute code and caputre any output or errors.
@@ -12,15 +17,37 @@ type Result struct {
 	TimedOut bool
 }
 
+// Exists jsut so we can put methods on it like RunPython
 type ExecutionService struct{}
 
+// Constructor, just returns a pointer to a new service.
 func NewExecutionService() *ExecutionService {
 	return &ExecutionService{}
 }
 
+// Prepares files and directory.
 func (s *ExecutionService) RunPython(code string) (Result, error) {
-	// make temp dir
-	// write script.py
-	// run docker with isolation flags
-	// return the Result
+	// Create new folder in the OS temp location. * is replaced by a random number.
+	dir, err := os.MkdirTemp("", "sandbox-*")
+	if err != nil {
+		return Result{}, err
+	}
+	// Schedules deletion of folder when func returns
+	defer os.RemoveAll(dir)
+
+	// Container user is UID 1000, which is not permitted to view it.
+	// Changes folders permissions so that everyone can read it
+	err = os.Chmod(dir, 0o755)
+	if err != nil {
+		return Result{}, err
+	}
+
+	// Creates script.py inside temp folder we created
+	// with updated permissions. so that only the contianer
+	// can read the file.
+	err = os.WriteFile(filepath.Join(dir, "script.py"), []byte(code), 0o644)
+	if err != nil {
+		return Result{}, err
+	}
+
 }
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"backend/internal/api/handler"
+	"backend/internal/api/router"
+	"backend/internal/service"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	svc := service.NewExecutionService()
+	h, err := handler.NewHandler(svc, 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := router.Router(h)
+
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
 }
