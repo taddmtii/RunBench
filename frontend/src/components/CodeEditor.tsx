78e7617261ef4d031d64a7a03eba4e966344c7c6
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-09-20 16:46:00 -0400

    docker files for cpp and csharp as well as added UI options for both

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -13,6 +13,8 @@ export default function CodeEditor() {
         { label: "Python", value: "python"},
         { label: "Typescript", value: "typescript"},
         { label: "Javascript", value: "javascript"},
+        { label: "C++", value: "cpp"},
+        { label: "C#", value: "csharp"},
     ]
 
     const handleRunClick = async () => {
